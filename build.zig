5ba8df0e65e39d5aa29d6b7196f271cd15a5d203
tungli <tun@mail.muni.cz>
2025-05-08 11:47:11 +0200

    correct build: now exports module

--- build.zig
+++ build.zig
@@ -1,24 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-    const mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const source_file = b.path("src/root.zig");
 
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "dheap",
-        .root_module = mod,
+    _ = b.addModule("dheap", .{
+        .root_source_file = source_file
     });
-
-    b.installArtifact(lib);
 
     const tests = b.addTest(.{
-        .root_module = mod,
+        .root_source_file = source_file,
     });
 
     const run_tests = b.addRunArtifact(tests);
